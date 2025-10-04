528dd54f48f397c46f424b00c80f23b92bb6e32a
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-10-04 12:06:27 +0530

    add kafka consumer to inventory

---
+++ inventory/src/main/java/com/Inventory/Inventory/kafka/OrderConsumer.java
@@ -0,0 +1,19 @@
+package com.Inventory.Inventory.kafka;
+
+import com.example.base.dto.OrderEventDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderEventDto.class);
+    @KafkaListener(
+            topics = "${spring.kafka.template.default-topic}",
+            groupId = "spring.kafka.consumer.group-id"
+    )
+    public void consume(OrderEventDto orderEventDto){
+        LOGGER.info("Receiving order event from the topic:{}",orderEventDto);
+    }
+}
