04749aba9b3fb1a626e746429bdbe9651b16bbbd
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-10-05 16:59:38 +0530

    update order consumer group-id value injection

--- inventory/src/main/java/com/Inventory/Inventory/kafka/OrderConsumer.java
+++ inventory/src/main/java/com/Inventory/Inventory/kafka/OrderConsumer.java
@@ -11,7 +11,7 @@ public class OrderConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderEventDto.class);
     @KafkaListener(
             topics = "${spring.kafka.template.default-topic}",
-            groupId = "spring.kafka.consumer.group-id"
+            groupId = "${spring.kafka.consumer.group-id}"
     )
     public void consume(OrderEventDto orderEventDto){
         LOGGER.info("Receiving order event from the topic:{}",orderEventDto);
